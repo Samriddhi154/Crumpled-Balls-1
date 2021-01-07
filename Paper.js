1dc5f2730f09ea1c45f178e61720e57b3327d5a7
Samriddhi154 <70470361+Samriddhi154@users.noreply.github.com>
2021-01-07 14:25:02 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -6,9 +6,11 @@ class Paper {
         friction:0.5,
         density:1.2
     }
+    
+    ellipse(x,y,w,[h])
+    
+    Matter.Bodies.circle(x,y,radius,[options],maxSides])
 
-    if(keyDown(upArrow)){
-
-    }
-
+    if(keyCode===UP_ARROW)){
+ Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
 }
